e48c92e4eb30d592efc9534b9b93ebc82a2a9d2f
FossFalling <fossfalling@gmail.com>
2025-11-25 07:09:08 +0700

    Add AudioManager baseline
    Implement initial AudioManager.cs handling background music and sound effects

---
+++ AudioManager.cs
@@ -0,0 +1,75 @@
+﻿        using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_Game
+{
+    public class AudioManager : IDisposable
+    {
+        private readonly IWavePlayer audioPlayer;
+        public readonly AudioFileReader audioReader;
+
+        public AudioManager(string filePath)
+        {
+            audioPlayer = new WaveOutEvent();
+            audioReader = new AudioFileReader(filePath);
+            audioPlayer.Init(audioReader);
+        }
+
+        public void Play()
+        {
+            audioReader.Position = 0; 
+            audioPlayer.Play();
+        }
+
+        public void Continue()
+        {
+            audioPlayer.Play();
+        }
+
+        public void Stop()
+        {
+            audioPlayer.Stop();
+        }
+
+        public void SetVolume(float volume)
+        {
+            audioPlayer.Volume = volume;
+        }
+
+        public PlaybackState GetPlaybackState()
+        {
+            return audioPlayer.PlaybackState;
+        }
+
+        public void Pause()
+        {
+            if (audioPlayer.PlaybackState == PlaybackState.Playing)
+            {
+                audioPlayer.Pause();
+            }
+        }
+
+        public void Resume()
+        {
+            if (audioPlayer.PlaybackState == PlaybackState.Paused)
+            {
+                audioPlayer.Play();
+            }
+        }
+
+        public void Reset()
+        {
+            audioReader.Position = 0;
+        }
+
+        public void Dispose()
+        {
+            audioPlayer.Dispose();
+            audioReader.Dispose();
+        }
+    }
+}
