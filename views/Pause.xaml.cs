2f874ff163b948b57d4176aaf9f97c360edd7e41
yoyozero10 <trandaithang2023@gmail.com>
2025-11-27 21:01:02 +0700

    Add Pause window with continue and home actions
    Introduces a new Pause window (Pause.xaml and Pause.xaml.cs) for the Tetris game. The window provides styled Continue and Home buttons, allowing users to resume the game or return to the home screen, with appropriate audio feedback and data saving.

---
+++ views/Pause.xaml.cs
@@ -0,0 +1,56 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tetris_Game
+{
+    /// <summary>
+    /// Interaction logic for Pause.xaml
+    /// </summary>
+    public partial class Pause : Window
+    {
+        private MainWindow mainWindow;
+        public Pause(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+
+        private void ButtonContinue_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.clickedAudio.Play();
+            this.Close();
+            mainWindow.PauseMenu.Visibility = Visibility.Collapsed;
+            mainWindow.ContinueGame();
+        }
+
+        private void ButtonHome_Click(object sender, RoutedEventArgs e)
+        {
+            if (mainWindow.IsMuted == true)
+            {
+                mainWindow.ButtonVolume_Click(mainWindow.ButtonVolume, null);
+            }
+            else
+            {
+                mainWindow.clickedAudio.Play();
+            }
+
+            mainWindow.SaveData();
+            InGame inGame = new InGame();
+            this.Close();
+            mainWindow.Close();
+            inGame.Show();
+        }
+    }
+}
