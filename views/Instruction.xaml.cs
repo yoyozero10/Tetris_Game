3a3ed5df99a08cb82e50103ead341825d3ca41c9
thangchua36-max <thangchua36@gmail.com>
2025-12-05 00:19:41 +0700

    Add Instruction window with exit button
    Introduces a new Instruction window in XAML with a custom-styled exit button. The corresponding code-behind plays a click sound and closes the window when the exit button is pressed.

---
+++ views/Instruction.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tetris_Game
+{
+    /// <summary>
+    /// Interaction logic for Instruction.xaml
+    /// </summary>
+    public partial class Instruction : Window
+    {
+        private readonly AudioManager clickedAudio;
+        public Instruction()
+        {
+            InitializeComponent();
+            clickedAudio = new AudioManager("resources/audio/ClickedAudio.wav");
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            clickedAudio.Play();
+            this.Close();
+        }
+    }
+}
